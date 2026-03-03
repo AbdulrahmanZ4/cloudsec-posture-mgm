7462baf80c85d99a6435c0b10c8dbcdecc76aad5
Abdulrahman <d7oom_z4@outlook.com>
2026-03-03 22:07:22 +0300

    enable VPC flow logs for lateral movement visibility

--- terraform/network.tf
+++ terraform/network.tf
@@ -8,6 +8,12 @@ resource "google_compute_subnetwork" "web_subnet" {
   ip_cidr_range = "10.10.10.0/24"
   region        = var.region
   network       = google_compute_network.vpc.id
+
+  log_config {
+    aggregation_interval = "INTERVAL_5_SEC"
+    flow_sampling        = 0.5
+    metadata             = "INCLUDE_ALL_METADATA"
+  }
 }
 
 resource "google_compute_subnetwork" "db_subnet" {
@@ -15,4 +21,10 @@ resource "google_compute_subnetwork" "db_subnet" {
   ip_cidr_range = "10.10.20.0/24"
   region        = var.region
   network       = google_compute_network.vpc.id
+
+  log_config {
+    aggregation_interval = "INTERVAL_5_SEC"
+    flow_sampling        = 0.5
+    metadata             = "INCLUDE_ALL_METADATA"
+  }
 }
